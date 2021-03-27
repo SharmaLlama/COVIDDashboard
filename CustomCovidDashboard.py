af24d915354c3a4a8b86eba7c14ada497d7fe723
Utkarsh Sharma <72532670+SharmaLlama@users.noreply.github.com>
2021-03-28 09:27:02 +1000

    Add files via upload

---
+++ CustomCovidDashboard.py
@@ -0,0 +1,26 @@
+import pil
+import streamlit as st
+import src.pages.about
+import src.pages.dataAnalysis
+import src.pages.dashboard
+import src.pages.homePage
+
+PAGE_PY = {
+    "Home": src.pages.homePage,
+    "About": src.pages.about,
+    "Statistics About COVID": src.pages.dataAnalysis,
+    "Dashboard": src.pages.dashboard
+}
+
+
+def main():
+    st.sidebar.title("Menu")
+    choices = st.sidebar.radio("Navigate", list(PAGE_PY.keys()))
+    PAGE_PY[choices].main()
+    st.sidebar.title("About")
+    st.sidebar.info("""
+    This simple dashboard is maintained by Utkarsh Sharma. You can learn more about me at []. """)
+
+
+if __name__ == "__main__":
+    main()
