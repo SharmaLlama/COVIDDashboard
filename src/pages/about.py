849ea7b15e4e4c98d9845ae919a4971a1fdec8fc
Utkarsh Sharma <72532670+SharmaLlama@users.noreply.github.com>
2021-03-28 19:50:03 +1000

    Update about.py

--- src/pages/about.py
+++ src/pages/about.py
@@ -4,4 +4,4 @@ import streamlit as st
 def main():
     st.title("About Me")
     st.info("""
-    This simple dashboard is maintained by Utkarsh Sharma. You can learn more about me at []. """)
+    This simple dashboard is maintained by Utkarsh Sharma. You can learn more about me at [https://github.com/SharmaLlama]. """)
